ce6b446ab9f908f9285bd52f239a8f86d5da4c59
Amir Ziai <arziai@gmail.com>
2021-03-11 17:19:50 -0800

    new repo works with cpu, will try gpu soon

--- experiment.py
+++ experiment.py
@@ -13,9 +13,8 @@ from ogb.graphproppred import PygGraphPropPredDataset, Evaluator
 from torch.autograd import Variable
 from tqdm import tqdm
 
-from gnn import GNN
-from model import GMN
 from dataset import DataLoaderGMN, DataLoaderGNN
+from gnn import GNN
 
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 logger = logging.getLogger(__name__)
@@ -115,6 +114,9 @@ class Experiment:
             self.valid_curve.append(valid_perf)
             self.test_curve.append(test_perf)
 
+            if self.stop_early:
+                break
+
         self.times["end"] = datetime.now()
         best_val_epoch = int(np.array(self.valid_curve).argmax())
         best_train = max(self.train_curve)
@@ -124,6 +126,10 @@ class Experiment:
         logger.info(f"Best valid : {self.valid_curve[best_val_epoch]}")
         logger.info(f"Best test  : {self.test_curve[best_val_epoch]}")
         self._store_results()
+
+    @property
+    def stop_early(self) -> bool:
+        return False
 
     @property
     def max_nodes(self):
@@ -287,6 +293,8 @@ class GMNExperiment(Experiment):
             num_workers,
             debug,
         )
+        from model import GMN
+
         self.num_centroids = num_centroids
         self.weight_decay = weight_decay
         self.decay_step = decay_step
@@ -453,9 +461,139 @@ class GMNExperiment(Experiment):
         return y_true, y_pred
 
 
+class GMNExperimentRethink(Experiment):
+    def __init__(
+        self,
+        dropout: float,
+        num_layers: int,
+        emb_dim: int,
+        epochs: int,
+        lr: float,
+        device: int,
+        batch_size: int,
+        num_workers: int,
+        num_heads: int,
+        hidden_dim: int,
+        num_keys: List[int],
+        mem_hidden_dim: int,
+        variant: str,
+        lr_decay_patience: int,
+        kl_period: int,
+        early_stop_patience: int,
+        debug: bool = False,
+    ):
+        super().__init__(
+            "gmn-rethink",
+            dropout,
+            num_layers,
+            emb_dim,
+            epochs,
+            lr,
+            device,
+            batch_size,
+            num_workers,
+            debug,
+        )
+        from data.datasets import get_data
+        from torch.optim import Adam
+        from torch.optim.lr_scheduler import ReduceLROnPlateau
+        from gmn.GMN import GMN
+
+        self.kl_period = kl_period
+        self.variant = variant
+        self.early_stop_patience = early_stop_patience
+
+        self.epochs_no_improve = 0
+        self.epoch_stop = self.DEBUG_BATCHES if self.debug else None
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        res = get_data(self.DATASET_NAME, batch_size)
+        train_loader, val_loader, test_loader, stats, evaluator, encode_edge = res
+        self.loaders = {"train": train_loader, "test": test_loader, "valid": val_loader}
+        self.model = GMN(
+            stats["num_features"],
+            stats["max_num_nodes"],
+            stats["num_classes"],
+            num_heads,
+            hidden_dim,
+            num_keys,
+            mem_hidden_dim,
+            variant=variant,
+            encode_edge=encode_edge,
+        ).to(self.device)
+        no_keys_param_list = [
+            param for name, param in self.model.named_parameters() if "keys" not in name
+        ]
+        self.optimizer = Adam(no_keys_param_list, lr=lr)
+        self.scheduler = ReduceLROnPlateau(
+            self.optimizer,
+            mode="max",
+            factor=0.5,
+            min_lr=1e-6,
+            patience=lr_decay_patience,
+        )
+        self.kl_optimizer = Adam(self.model.parameters(), lr=lr)
+        self.kl_scheduler = ReduceLROnPlateau(
+            self.kl_optimizer,
+            mode="max",
+            factor=0.5,
+            min_lr=1e-6,
+            patience=lr_decay_patience,
+        )
+
+    @property
+    def stop_early(self) -> bool:
+        if self.epochs_no_improve >= self.early_stop_patience:
+            logging.info("Hit early stopping!")
+            return True
+        return False
+
+    def _train(self):
+        from gmn.train import train, kl_train
+
+        trainer = (
+            kl_train
+            if self.epoch % self.kl_period == 0 and self.variant == "gmn"
+            else train
+        )
+        train_sup_loss, train_kl_loss = trainer(
+            self.model,
+            self.kl_optimizer,
+            self.loaders["train"],
+            self.device,
+            self.epoch_stop,
+        )
+        return train_sup_loss
+
+    def _eval(self, data_split: str):
+        from gmn.train import evaluate
+
+        loader = self.loaders[data_split]
+        acc, _, _ = evaluate(
+            self.model,
+            loader,
+            self.device,
+            evaluator=self.evaluator,
+            data_split=data_split,
+            epoch_stop=self.epoch_stop,
+        )
+        if data_split == "valid":
+            self.scheduler.step(acc)
+
+            if (
+                self.epoch > 2
+                and self.valid_curve[-1]
+                <= self.valid_curve[-2 - self.epochs_no_improve]
+            ):
+                self.epochs_no_improve += 1
+            else:
+                self.epochs_no_improve = 0
+
+        return acc
+
+
 if __name__ == "__main__":
-    gmn = True
-    if not gmn:
+    which = "rethink"  # or gnn or gmn
+    if which == "gnn":
         exp = Experiment(
             gnn_type="gin",
             dropout=0.5,
@@ -468,9 +606,8 @@ if __name__ == "__main__":
             num_workers=0,  # everything in the main process
             debug=True,
         )
-        exp.run()
-    else:
-        exp_gmn = GMNExperiment(
+    elif which == "gmn":
+        exp = GMNExperiment(
             dropout=0.5,
             num_layers=5,
             emb_dim=10,
@@ -502,4 +639,27 @@ if __name__ == "__main__":
             linear_block=False,
             debug=False,
         )
-        exp_gmn.run()
+    elif which == "rethink":
+        exp = GMNExperimentRethink(
+            dropout=0.5,
+            num_layers=5,
+            emb_dim=300,
+            epochs=1000,
+            lr=1e-3,
+            device=0,
+            batch_size=128,
+            num_workers=0,  # everything in the main process
+            num_heads=5,
+            hidden_dim=64,
+            num_keys=[32, 1],
+            mem_hidden_dim=16,
+            variant="gmn",
+            lr_decay_patience=10,
+            kl_period=5,
+            early_stop_patience=50,
+            debug=True,
+        )
+    else:
+        raise ValueError("experiment type not defined!")
+
+    exp.run()
